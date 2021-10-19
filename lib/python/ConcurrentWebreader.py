1d423726f705e143409e04d4146a178cbdb30879
Yaguang Zhang <yaguangzhang@outlook.com>
2021-10-19 10:19:28 -0400

    Deal error in the python web reader.

--- lib/python/ConcurrentWebreader.py
+++ lib/python/ConcurrentWebreader.py
@@ -5,6 +5,9 @@
 import threading
 import urllib.request
 import ssl
+from socket import timeout
+from urllib.error import HTTPError, URLError
+import logging
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -12,7 +15,17 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 def fetch_url(url, htmls, idx):
-    htmls[idx] = urllib.request.urlopen(url, context=ctx).read()
+    TIMEOUT_IN_S = 10
+
+    try:
+        htmls[idx] = urllib.request.urlopen(url,
+            timeout=TIMEOUT_IN_S, context=ctx).read()
+    except (HTTPError, URLError) as error:
+        logging.error('Data not retrieved because %s\nURL: %s', error, url)
+    except timeout:
+        logging.error('Socket timed out - URL %s', url)
+    else:
+        logging.info('Access successful.')
 
 def concurrent_fetch_urls(urls):
     numOfUrls = len(urls)
@@ -33,6 +46,7 @@ def concurrent_fetch_urls(urls):
 if __name__ == '__main__':
     # For testing.
     urls = ["https://www.google.com", "http://www.yahoo.com", "http://www.baidu.com", \
-        "https://nationalmap.gov/epqs/pqs.php?x=-80&y=42&units=Meters&output=json"]
+        "https://nationalmap.gov/epqs/pqs.php?x=-80&y=42&units=Meters&output=json", \
+        "http://artsy.com"]
     htmls = concurrent_fetch_urls(urls)
     print('Successfully fetched', sum(h is not None for h in htmls), 'out of', len(urls), 'URLs!')
