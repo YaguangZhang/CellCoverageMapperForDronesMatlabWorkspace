2a1fcfa8788d0fe97e96c65c51c7c1d764ee9494
Yaguang Zhang <yaguangzhang@outlook.com>
2021-12-09 13:18:35 -0500

    Add sim groups for sim man.

--- runMultipleCellCovSims.m
+++ runMultipleCellCovSims.m
@@ -18,20 +18,41 @@ pathToSaveSimManDiary = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
     'PostProcessingResults', 'simManDiary.txt');
 diary(pathToSaveSimManDiary);
 
-% Presets of interest.
-%   PRESETS = {'Tipp', 'ShrinkedWHIN', 'ShrinkedIN'};
-PRESETS = {'ShrinkedWHIN'};
+% Shortcuts for predefined simulation groups.
+%   - 'cellularCov'
+%     Large-scale cellular coverage simulations.
+%   - 'whinLoRaWan'
+%     The WHIN LoRaWAN + WHIN weather stations.
+%   - 'acreLoRaWan'
+%     The ACRE LoRaWAN.
+SIM_GROUP_PRESET = 'acreLoRaWan';
 
-% Carrier frequencies.
-%	- 1900 MHz
-%     For cellular 4G LTE
-%   - C-Band: 3700 MHz (band n77) and 4700 MHz (band n79)
-%     For cellular 5G sub 6G
-%   - 7000 MHz (7 GHz) and 13000 MHz (13GHz)
-%     For broadband wireless backhaul.
-%   - mmWave 28000 MHz (28 GHz)
-%     For cellular 5G millimeter wave
-CARRIER_FREQUENCIES_IN_MHZ = {1900, 3700, 4700, 7000, 13000, 28000};
+switch SIM_GROUP_PRESET
+    case 'cellularCov'
+        % Presets of interest.
+        PRESETS = {'Tipp', 'ShrinkedWHIN', 'ShrinkedIN'};
+
+        % Carrier frequencies.
+        %	- 1900 MHz
+        %     For cellular 4G LTE
+        %   - C-Band: 3700 MHz (band n77) and 4700 MHz (band n79)
+        %     For cellular 5G sub 6G
+        %   - 7000 MHz (7 GHz) and 13000 MHz (13GHz)
+        %     For broadband wireless backhaul.
+        %   - mmWave 28000 MHz (28 GHz)
+        %     For cellular 5G millimeter wave
+        CARRIER_FREQUENCIES_IN_MHZ ...
+            = {1900, 3700, 4700, 7000, 13000, 28000};
+    case 'whinLoRaWan'
+        PRESETS = {'WHIN_WEATHER_STATIONS', 'WHIN_LORAWAN'};
+        CARRIER_FREQUENCIES_IN_MHZ = {915};
+    case 'acreLoRaWan'
+        PRESETS = {'ACRE_LORA_5MILE_R', 'ACRE_LORA_1MILE_R', ...
+            'ACRE_LORA_HALF_MILE_R'};
+        CARRIER_FREQUENCIES_IN_MHZ = {915};
+    otherwise
+        error(['Unknown simulation group: ', SIM_GROUP_PRESET, '!']);
+end
 
 for idxFre = 1:length(CARRIER_FREQUENCIES_IN_MHZ)
     for idxPreset = 1:length(PRESETS)
