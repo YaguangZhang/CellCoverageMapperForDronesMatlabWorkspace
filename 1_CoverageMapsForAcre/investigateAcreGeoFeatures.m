fa2b72a3d497d208fb0e3dc1521fa203a8999522
Yaguang Zhang <yaguangzhang@outlook.com>
2022-03-16 15:54:39 -0400

    Add LiDAR data extraction for ACRE.

--- 1_CoverageMapsForAcre/investigateAcreGeoFeatures.m
+++ 1_CoverageMapsForAcre/investigateAcreGeoFeatures.m
@@ -13,31 +13,96 @@ curFileName = mfilename;
 
 prepareSimulationEnv;
 
-%% Load Boundary
+% The absolute path to the folder for saving the results.
+pathToSaveResults = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
+    'PostProcessingResults', 'AcreGeoFeatureInvestigation');
+if ~exist(pathToSaveResults, 'dir')
+    mkdir(pathToSaveResults)
+end
 
-absDirToAcreKmz = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
-    'Lidar', 'ACRE', 'AcreExactBoundaryRaw', 'ACRE.kmz');
+% We will load cached results for faster processing if available.
+dirToCachedResults = fullfile(pathToSaveResults, 'cachedResults.mat');
+if exist(dirToCachedResults, 'file')
+    load(dirToCachedResults);
+else
+    %% Load Boundary
+    absDirToAcreKmz = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
+        'Lidar', 'ACRE', 'AcreExactBoundaryRaw', 'ACRE.kmz');
 
-% Boundary of ACRE.
-[UTM_X_Y_BOUNDARY_ACRE, UTM_ZONE_ACRE, kmzStruct, ...
-    utmXYPolygons, lonLatPolygons] ...
-    = extractBoundaryFromKmzFile(absDirToAcreKmz);
+    % Boundary of ACRE.
+    [UTM_X_Y_BOUNDARY_ACRE, UTM_ZONE_ACRE, acreKmzStruct, ...
+        utmXYAcrePolygons, lonLatAcrePolygons] ...
+        = extractBoundaryFromKmzFile(absDirToAcreKmz);
 
-% For GPS and UTM conversions.
-[deg2utm_speZone, utm2deg_speZone] ...
-    = genUtmConvertersForFixedZone(UTM_ZONE_ACRE);
+    % For GPS and UTM conversions.
+    [deg2utm_speZone, utm2deg_speZone] ...
+        = genUtmConvertersForFixedZone(UTM_ZONE_ACRE);
 
-UTM_LAT_LON_BOUNDARY_ACRE = nan(size(UTM_X_Y_BOUNDARY_ACRE));
-[UTM_LAT_LON_BOUNDARY_ACRE(:,1), UTM_LAT_LON_BOUNDARY_ACRE(:,2)] ...
-    = utm2deg_speZone(UTM_X_Y_BOUNDARY_ACRE(:,1), ...
-    UTM_X_Y_BOUNDARY_ACRE(:,2));
+    UTM_LAT_LON_BOUNDARY_ACRE = nan(size(UTM_X_Y_BOUNDARY_ACRE));
+    [UTM_LAT_LON_BOUNDARY_ACRE(:,1), UTM_LAT_LON_BOUNDARY_ACRE(:,2)] ...
+        = utm2deg_speZone(UTM_X_Y_BOUNDARY_ACRE(:,1), ...
+        UTM_X_Y_BOUNDARY_ACRE(:,2));
 
-%% Create a Grid
+    %% Create a Grid
 
-% Corresponds to the 5 feet resolution of the LiDAR DSM.
-gridResolutionInM = 1.5;
+    % Corresponds to the 5 feet resolution of the LiDAR DSM.
+    gridResolutionInM = 1.5;
+    gridUtmXYPts = buildSimGrid(UTM_X_Y_BOUNDARY_ACRE, ...
+        gridResolutionInM, true);
 
-gridUtmXYPts = buildSimGrid(UTM_X_Y_BOUNDARY_ACRE, ...
-    gridResolutionInM, true);
+    %% LiDAR Data Info
+    dirToLidarFiles = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
+        'Lidar_2019', 'IN', 'DSM');
+
+    % Preprocess .img/.tif LiDAR data. To make Matlab R2019b work, we need
+    % to remove preprocessIndianaLidarDataSet from path after things are
+    % done.
+    addpath(fullfile(pwd, 'lib', 'lidar'));
+    [lidarFileRelDirs, lidarFileXYCoveragePolyshapes, ~] ...
+        = preprocessIndianaLidarDataSetDsm(dirToLidarFiles, ...
+        deg2utm_speZone, utm2deg_speZone);
+    rmpath(fullfile(pwd, 'lib', 'lidar'));
+    lidarFileAbsDirs = cellfun(@(d) ...
+        [dirToLidarFiles, strrep(d, '\', filesep)], ...
+        lidarFileRelDirs, 'UniformOutput', false);
+
+    % Extra information on the LiDAR data set.
+    %   - Overall boundries for the area covered by the LiDAR data set in
+    %   UTM.
+    % lidarFilesXYCoveragePolyshape ...
+    %     = mergePolygonsForAreaOfInterest(lidarFileXYCoveragePolyshapes,
+    %     1);
+    %   - Centroids for the LiDAR files in UTM.
+    lidarFileXYCentroids ...
+        = extractCentroidsFrom2DPolyCell(lidarFileXYCoveragePolyshapes);
+    %   - The .mat copies for the LiDAR data. For the 2019 dataset, they
+    %   are stored in a cache folder.
+    lidarMatFileAbsDirs = lidarFileAbsDirs;
+    for idxMatF = 1:length(lidarMatFileAbsDirs)
+        [lidarMatFPath, lidarMatFName, ~] ...
+            = fileparts(lidarMatFileAbsDirs{idxMatF});
+        lidarMatFileAbsDirs{idxMatF} = fullfile(lidarMatFPath, '..', ...
+            'MatlabCache', [lidarMatFName, '.mat']);
+    end
+
+    %% Fetch LiDAR Data
+
+    [terrainEles, lidarZs, curEleForNanPts] ...
+        = generateProfileSamps( ...
+        UTM_X_Y_BOUNDARY_ACRE, utm2deg_speZone, ...
+        lidarFileXYCentroids, lidarFileXYCoveragePolyshapes, ...
+        lidarMatFileAbsDirs, 'both');
+
+    save(dirToCachedResults, ...
+        ... ACRE field boudnaries:
+        'UTM_X_Y_BOUNDARY_ACRE', 'UTM_ZONE_ACRE', 'acreKmzStruct', ...
+        'utmXYAcrePolygons', 'lonLatAcrePolygons', ...
+        ... Grid point locs:
+        'gridResolutionInM', 'gridUtmXYPts', ...
+        ... Ground elevation and LiDAR z values.
+        'terrainEles', 'lidarZs');
+end
+
+%% Plots
 
 % EOF
