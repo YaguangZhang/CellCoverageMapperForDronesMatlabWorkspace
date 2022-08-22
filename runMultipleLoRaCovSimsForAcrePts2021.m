97e2ab532e522aa019e1f5821cd849c20a4ada21
Yaguang Zhang <yaguangzhang@outlook.com>
2022-08-22 16:17:28 -0400

    Add simulation for Ag IT version of ACRE 2021 data.

--- runMultipleLoRaCovSimsForAcrePts2021.m
+++ runMultipleLoRaCovSimsForAcrePts2021.m
@@ -23,8 +23,12 @@ disp(['[', datestr(now, datetimeFormat), ...
 % Predefined simulation group label.
 %   - 'acreLoRaWan2021'
 %     The ACRE LoRaWAN results collected in 2021: one tower gateway +
-%     multiple vechiles.
-SIM_GROUP_PRESET = 'AcreLoRaWan2021';
+%     multiple vechiles. Based on ThingsBoard records.
+%   - 'acreLoRaWan2021AgIT'
+%     Same as 'acreLoRaWan2021', except that the data source is Purdue Ag
+%     IT, which powers ThingsBoard and is in fact more complete (with less
+%     missing points).
+SIM_GROUP_PRESET = 'acreLoRaWan2021AgIT';
 
 pathToPostProcessingResultsFolder ...
     = fullfile(ABS_PATH_TO_SHARED_FOLDER, ...
@@ -92,9 +96,19 @@ disp(' ')
 disp(['[', datestr(now, datetimeFormat), ...
     '] Setting up simulations ...'])
 
-ABS_PATH_TO_RX_LOCS = fullfile( ...
-    ABS_PATH_TO_SHARED_FOLDER, ...
-    'AcreRxInfo', '2021', 'oyster_data_for_sim.csv');
+switch SIM_GROUP_PRESET
+    case 'acreLoRaWan2021'
+        ABS_PATH_TO_RX_LOCS = fullfile( ...
+            ABS_PATH_TO_SHARED_FOLDER, ...
+            'AcreRxInfo', '2021', 'oyster_data_for_sim.csv');
+    case 'acreLoRaWan2021AgIT'
+        ABS_PATH_TO_RX_LOCS = fullfile( ...
+            ABS_PATH_TO_SHARED_FOLDER, ...
+            'AcreRxInfo', '2021', 'oyster_data_ag_it.csv');
+    otherwise
+        error(['Unsupported SIM_GROUP_PRESET: ', SIM_GROUP_PRESET, '!']);
+end
+
 rxGidLonLatHInMs = readmatrix(ABS_PATH_TO_RX_LOCS);
 
 rxHInMsUnique = unique(rxGidLonLatHInMs(:,4));
